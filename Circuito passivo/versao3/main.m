aca819618f553f0f9f6d246366339d6418a829fc
Rafael Corsi <corsiferrao@gmail.com>
2015-02-20 17:53:23 -0200

    Fx comsol = 20.1N e modelo = 20.8N

--- Circuito passivo/versao3/main.m
+++ Circuito passivo/versao3/main.m
@@ -1,11 +1,10 @@
-clear
+%limpa variaveis
+clear;
+close all;
 
 % Inicialização
 parametros;
 load iron;
-
-% entreferro
-wge = wge - 0E-3;
 
 % Valor inicial
 Hef = 3E4;
@@ -13,61 +12,76 @@ Hrf = 3E4;
 Hrr = 3E4;
 
 % loop para convergencia
-for i=1:50
+% método de Newton
+for i=1:40
 
-    % Valores iniciais dos coeficiente
+    % Valores iniciais das permeabilidades
     uef = iron.MuH(Hef);
     urf = iron.MuH(Hrf);
     urr = iron.MuH(Hrr);
 
     % Relutancias
-    Rm  = hm/(um*Sm);
-    Ref = wef/(uef*Sef);
-    Rge = wge/(u0*Sge);
-    Rre = wrf/(urf*Srf);
-    Rrr = hm/(urr*Srr);
-    
-    SumR = Rm+2*Ref+2*Rge+2*Rre+Rrr;
- 
-    
-    Rml = (0.318*pee*log(1+2*(ree*0.050)/pee))/u0;
-    Rms = 0.318*pee/u0;
+    Rp  = hm/(um*Sm);               % Ima
+    Ref = wef/(uef*Sef);            % estator externo
+    Rrf = wrf/(urf*Srf);            % rotor ferro
+    Rrr = hm/(urr*Srr);             % rotor retorno
+    Rge = wge/(u0*Sge);             % gap
 
-    SumL = Rml+Rms;
+    % Permeace de leakage no gap de acordo com 
+    % http://product.tdk.com/en/products/magnet/pdf/e371_circuit.pdf
+
+    Pgl1 = 0.264*u0*pei;
+    Pgl2 = 0.64*u0*pei/(wge/(wef*0.3)+1);
+    Pgl3 = 0.077*u0*wge;
+    Pgl4 = u0*wef*0.3/4;
     
-    phym =     
+    SumRg = 1/(Pgl1+Pgl2+Pgl3+Pgl4+inv(Rge)); % associacao //
+            
+    % Permeace de leakage no ima de acordo com 
+    % http://product.tdk.com/en/products/magnet/pdf/e371_circuit.pdf    
+    Pl2 = 0.64*u0*ree/(hm/(hm+2*hef)+1); 
+    Rl = 1/Pl2;                             
     
-    
-    % Cálculo do fluxo magnético
-    Fc   = (Hc*hm);
-    phym = 0.8*Fc/SumR;
+    % analise de circuito
+    SumF = 2*Ref+2*SumRg+2*Rrf+Rrr; % Associacao série circutio gap
+    RLF  = SumF*Rl/(SumF+Rl);       % Associacao // entre SumL e Rl
+    Fc   = Hc*hm;                   % Coercive magnetic force
+
+    % fluxo magnético
+    phym = Fc/(RLF+Rp);             % ima
+    phyf = RLF*phym/SumF;            % circuito gap
+    phyl = RLF*phym/Rl;              % leakage
+    phyg = phyf*SumRg/Rge;          % gap
+
 
     % Calculo do vetor campo mag. nos componentes do sistem    
     Bm      = phym/Sm;
-    Bef     = phym/Sef;
-    Bge(i)  = phym/Sge;
-    Brf     = phym/Srf;
-    Brr     = phym/Srr;
+    Bef     = phyf/Sef;
+    Bge(i)  = phyg/Sge;
+    Brf     = phyf/Srf;
+    Brr     = phyf/Srr;
        
     % Atualiza valor do campo magnético
+    % via método de newton
     Hef = Bef/uef/2 + Hef/2;  % - H->B
     Hrf = Brf/urf/2 + Hrf/2;
     Hrr = Brr/urr/2 + Hrr/2;
 end
 
 
+%% Força de atração
 
-Fx = 2*(Bge(i)^2*Sge)/(2*u0);
+Fx = 2*(Bge(i)^2*Sge)/(2*u0)
 
-% campo magnetico ima
+%% campo magnetico ima e pnt de operação
+
 Hm = (Bm-Br)*Hc/Br;
-
-%%
 figure
     plot(Bge, 'o');
-    title('Convergencia gap');
+    axis([0 i 0 2]);
+    title('Convergencia B no gap');
 
-%%
+%% Convergência 
 figure
 hold on
     plot(-Hc:0,Br+Br/Hc.*(-Hc:0));
--- Circuito passivo/versao3/parametros.m
+++ Circuito passivo/versao3/parametros.m
@@ -2,14 +2,16 @@
 
 NFRAC = 36;     % Fraçoes de mancal (partes para o calculo)
                 % nfrac = 36 representa fraçoes de 10 graus
+                
 AS    = 1.2;    % Fator de espraiamento da área (entrefeerro)
 
 Hc  = 995000;   % Hc ima (representa aberto)
                 % para igualar ao modelo do comsol
                 % pelo datasheet = 1054E3
-Br  = 1.39;     % Br ima (representa curto)
+Br  = 1.2504;   % Br ima (representa curto)
+                % datasheet Br = 1.39
 
-hef  = 4E-3;    % Altura ferro
+hef  = 4E-3;    % Altura ferro estator externo
 wef  = 14E-3;   % Largura do ferro estator externo
 
 wm   = 8E-3;    % Largura imã
@@ -21,12 +23,13 @@ hge  = hef;     % Algura do ferro
 wrf  = 10E-3;   % Largura ferro rotor
 wrr  = 6E-3;    % Largura rotor retorno
 
-ree  = 75E-3;   % Raio ext nerno
+ree  = 75E-3;   % Raio externo estator
 rei  = ree-wef; % Raio interno ferro estator externo
-rre  = rei-wge; % Raio rotor externo
-rri  = rre-wrf; % Raio rotor interno
+rre  = rei-wge; % Raio rotor externo ferro
+rri  = rre-wrf; % Raio rotor interno ferro
 
-pee = 2*pi*ree/NFRAC; % perímetro raio ext
+pei = 2*pi*rei/NFRAC; % perímetro raio interno estator 
+pee = 2*pi*ree/NFRAC; % perímetro raio externo estator 
 
 Sm  = pi*(ree^2 - (ree-wm)^2)/NFRAC;     % Área da seccao ímã            (validado)
 Sef = 2*pi*(ree - wef/2)*hef/NFRAC;      % Área do ferro estator externo (42.58)
@@ -34,9 +37,10 @@ Srf = 2*pi*(rre - wrf/2)*hef/NFRAC;      % Area do ferro rotor           (41.74)
 Srr = pi*((rri+wrr)^2-rri^2)/NFRAC;      % Area do retorno rotor         (validado)
 Sge = AS*2*pi*(ree - wge/2)*hge/NFRAC;   % Area do gap
 
-%Sef = 42.58E-6;
-%Srf = 41.74E-6;
-%Sge = Sef*1.2;
+Sef = 42E-6;
+Srf = Sef;
+Sge = Sef;
+
 
 %% Permeabilidades
 
