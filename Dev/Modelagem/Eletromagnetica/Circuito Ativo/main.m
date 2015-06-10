13e9b8f9ccad552ae642cdc3c517c9cc51a2ff8c
Rafael Corsi <corsiferrao@gmail.com  git config --global user.name Your>
2015-06-09 22:29:55 -0300

    migrado resolucao de script main.m para funcao

--- Dev/Modelagem/Eletromagnetica/Circuito Ativo/main.m
+++ Dev/Modelagem/Eletromagnetica/Circuito Ativo/main.m
@@ -5,112 +5,28 @@
 clc
 clear all
 
-%% Parametros
-
-load iron;
-
-parametros_geometricos;
-parametros_magneticos;
 
 %% inicializacao
 % Deslocamentos relativos
-dx = 0;
 dy = 0;
-I  = [10 0 0 0 0 0 0 0 0 0];
+x=1;
+for Im=0:0.5:4
+    for dx=-0.3:0.1:0.3
+        I  = [Im Im/2 0 0 0 0 0 0 0 Im/2];
+        [Fx(x) Fy(x)] = resolve_ativo(dx,dy,I);
+        x = x+1;
+    end
+end
 
-% Forcas eletromotriz
-F = I*m.nnb;
+%%
+dx=0;
+Im = 2;
+I  = [Im Im/2 0 0 0 0 0 0 0 Im/2];
+resolve_ativo(dx,dy,I);
 
-% derivados
-m = derivados_geometricos(m,dx,dy);
-
-% Permeabilidade inicial do rotor
-ufr0 = 4E4;
-ufn0 = 4E4;
-uff0 = 4E4;
-
-%      AB    BC   CD   DE   EF   FG   GH   HA
-ufr = [ufr0 ufr0 ufr0 ufr0 ufr0 ufr0 ufr0 ufr0]';
-ufn = [ufn0 ufn0 ufn0 ufn0 ufn0 ufn0 ufn0 ufn0]';
-uff = [uff0 uff0 uff0 uff0 uff0 uff0 uff0 uff0]';
-% Relutancais
-
-% calcula gap com base no deslocamento x e y do rotor
-lg =lgap(m);
-lr = m.prr/8;
-lf = m.peie/8;
-ln = m.wnb;
-
-% calcula relutancia gaps
-Rg = Rmm(lg,m.Snbe,mag.u0);
-
-%% inicio interação
-ufn0 = ufn;
-ufr0 = ufr;
-uff0 = uff;
-
-% loop para convergencia
-% método de Newton
-for i=1:300
-    
-    if(i>1)
-        Hn0 = Hn;
-        Hr0 = Hr;
-        Hf0 = Hf;
-    end;
-    
-    % calcula relutancais rotor
-    Rr = Rmm(lr,m.Srr, ufr);
-    
-    % calcula relutancais nucleo
-    Rn = Rmm(ln, m.Snbe, ufn);
-    
-    % calcula retorno estator
-    Rf = Rmm(lf, m.Sei, uff);
-    
-    % resolve malhas
-    malhas;
-    
-    % Vetor campo mag. calcula campos nos componentes
-    Bg = abs(phi/m.Sgi/1.1);
-    Bn = abs(phi/m.Snbe);
-    Br = abs(I/m.Srrr);
-    Bf = abs(I/m.Sei);
-    
-    Hg = Bg/mag.u0;
-    Hn = Bn'/diag(ufn);
-    Hr = Br'/diag(ufr);
-    Hf = Bf'/diag(uff);
-    
-    % Atualiza permeabilidades
-    % via método de newton-rapson
-    if i==1
-        ufn = iron.MuH(Hn)/2 + ufn/2;
-        ufr = iron.MuH(Hr)/2 + ufr/2;
-        uff = iron.MuH(Hf)/2 + uff/2;
-    else
-        ufn = ufn + (Hn-Hn0)'./(ufn-ufn0);
-        ufr = ufr + (Hr-Hr0)'./(ufr-ufr0);
-        uff = uff + (Hf-Hf0)'./(uff-uff0);
-    end;
-
-    auxHr(i) = Hr(1);
-    auxHn(i) = Hn(1);
-    auxHf(i) = Hf(1);
-    
-end;
-
-%% Força
-
-projecao = diag([1 0.7071 1 0.7071 1 0.7071 1 0.7071]);
-
-Fmag = ((Bg.^2*m.Sgi)/(2*mag.u0))'*projecao;
-
-Fx = Fmag(1) + Fmag(2) - Fmag(4) - Fmag(5) -Fmag(6) + Fmag(8)
-
-Fy = -Fmag(2)-Fmag(3)-Fmag(4)+Fmag(6)+Fmag(7)+Fmag(8)
-
-figure; plot(auxHr)
-figure; plot(auxHn, 'g')
-figure; plot(auxHf, 'b')
-
+%%
+load Comsol
+figure
+plot(Comsol(:,3), 'x');
+hold
+plot(Fx, 'o');
---
+++ Dev/Modelagem/Eletromagnetica/Circuito Ativo/resolve_ativo.m
@@ -0,0 +1,114 @@
+function [ Fx, Fy ] = resolve_ativo(dx,dy,I)
+
+%% Parametros
+
+load iron;
+
+parametros_geometricos;
+parametros_magneticos;
+
+%% inicializacao
+
+% derivados
+m = derivados_geometricos(m,dx,dy);
+
+% Forcas eletromotriz
+F = I*m.nnb;
+
+% Permeabilidade inicial do rotor
+ufr0 = 4E4;
+ufn0 = 4E4;
+uff0 = 4E4;
+
+%      AB    BC   CD   DE   EF   FG   GH   HA
+ufr = [ufr0 ufr0 ufr0 ufr0 ufr0 ufr0 ufr0 ufr0]';
+ufn = [ufn0 ufn0 ufn0 ufn0 ufn0 ufn0 ufn0 ufn0]';
+uff = [uff0 uff0 uff0 uff0 uff0 uff0 uff0 uff0]';
+% Relutancais
+
+% calcula gap com base no deslocamento x e y do rotor
+lg =lgap(m);
+lr = m.prr/8;
+lf = m.peie/8;
+ln = m.wnb;
+
+% calcula relutancia gaps
+Rg = Rmm(lg,m.Snbe,mag.u0);
+
+%% inicio interação
+ufn0 = ufn;
+ufr0 = ufr;
+uff0 = uff;
+
+% loop para convergencia
+% método de Newton
+for i=1:20
+    
+    if(i>1)
+        Hn0 = Hn;
+        Hr0 = Hr;
+        Hf0 = Hf;
+    end;
+    
+    % calcula relutancais rotor
+    Rr = Rmm(lr,m.Srr, ufr);
+    
+    % calcula relutancais nucleo
+    Rn = Rmm(ln, m.Snbe, ufn);
+    
+    % calcula retorno estator
+    Rf = Rmm(lf, m.Sei, uff);
+    
+    % resolve malhas
+    malhas;
+    
+    % Vetor campo mag. calcula campos nos componentes
+    Bg = abs(phi/m.Sgi/1.1);
+    Bn = abs(phi/m.Snbe);
+    Br = abs(I/m.Srrr);
+    Bf = abs(I/m.Sei);
+    
+    Hg = Bg/mag.u0;
+    Hn = Bn'/diag(ufn);
+    Hr = Br'/diag(ufr);
+    Hf = Bf'/diag(uff);
+    
+    % Atualiza permeabilidades
+    % via método de newton-rapson
+    if i==1
+        ufn = iron.MuH(Hn)/2 + ufn/2;
+        ufr = iron.MuH(Hr)/2 + ufr/2;
+        uff = iron.MuH(Hf)/2 + uff/2;
+    else
+        ufn = ufn + (Hn-Hn0)'./(ufn-ufn0);
+        ufr = ufr + (Hr-Hr0)'./(ufr-ufr0);
+        uff = uff + (Hf-Hf0)'./(uff-uff0);
+    end;
+
+    auxHr{i} = Hr;
+    auxHn{i} = Hn;
+    auxHf{i} = Hf;
+    
+end;
+
+%% Força
+
+projecao = diag([1 0.7071 1 0.7071 1 0.7071 1 0.7071]);
+
+Fmag = ((Bg.^2*m.Sgi)/(2*mag.u0))'*projecao;
+
+Fx = Fmag(1) + Fmag(2) - Fmag(4) - Fmag(5) -Fmag(6) + Fmag(8);
+
+Fy = -Fmag(2)-Fmag(3)-Fmag(4)+Fmag(6)+Fmag(7)+Fmag(8);
+
+
+% figure;
+% plot(cellfun(@(x) x(1), auxHr), '.')
+% figure
+% plot(cellfun(@(x) x(1), auxHn), '.')
+% figure
+% plot(cellfun(@(x) x(1), auxHf), '.')
+
+
+end
+
